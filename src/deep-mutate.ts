5feccf61627b72db7ae1f1f3013ce80500fbcfdf
Vamshi Krishna Pothuka <pvamshi@gmail.com>
2019-03-05 13:11:03 +0530

    Add Object deep mutate

--- src/deep-mutate.ts
+++ src/deep-mutate.ts
@@ -1,6 +1,45 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+// RecursivePartial handling array too
 
+// export type RecursivePartial<T> = {
+//   [P in keyof T]?: T[P] extends (infer U)[]
+//     ? RecursivePartial<U>[]
+//     : T[P] extends object
+//     ? RecursivePartial<T[P]>
+//     : T[P]
+// }
+
+export type RecursivePartial<T> = { [P in keyof T]?: RecursivePartial<T[P]> }
+
+function mutate<T>(obj: T, partial: RecursivePartial<T> | undefined | null): T {
+  if (!partial || !obj) {
+    return obj
+  }
+
+  const updated: any = {}
+  for (const key in partial) {
+    if (partial.hasOwnProperty(key)) {
+      let element = partial[key]
+      if (
+        // tslint:disable-next-line: strict-type-predicates
+        typeof element === 'undefined'
+      ) {
+        updated[key] = undefined
+      } else if (
+        // tslint:disable-next-line: strict-type-predicates
+        typeof element === 'string' ||
+        // tslint:disable-next-line: strict-type-predicates
+        typeof element === 'number' ||
+        element instanceof String ||
+        element instanceof Number ||
+        Array.isArray(element)
+      ) {
+        updated[key] = element
+      } else {
+        updated[key] = mutate(obj[key], element)
+      }
+    }
+  }
+  return Object.freeze({ ...obj, ...updated })
 }
+
+export default mutate
--- test/deep-mutate.test.ts
+++ test/deep-mutate.test.ts
@@ -1,14 +1,92 @@
-import DummyClass from "../src/deep-mutate"
+import mutate from '../src/deep-mutate'
 
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
+describe('mutate test', () => {
+  it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  test('First level obj update', () => {
+    const frozen = Object.freeze({ name: 'foo', lastname: 'bar' })
+    const result = mutate(frozen, { name: 'faa' })
+    expect(result.name).toBe('faa')
+    expect(Object.isFrozen(result)).toBeTruthy()
+  })
+  test('Second level obj update', () => {
+    const frozen = Object.freeze({
+      name: 'Bruce',
+      lastname: 'Waine',
+      address: { city: 'Gotham', country: 'US', street: 'Mountain Drive' }
+    })
+    const result = mutate(frozen, { name: 'faa', address: { street: '1007 Mountain Drive' } })
+    expect(result.name).toBe('faa')
+    expect(result.address.street).toBe('1007 Mountain Drive')
+    expect(result).toEqual({
+      name: 'faa',
+      lastname: 'Waine',
+      address: { city: 'Gotham', country: 'US', street: '1007 Mountain Drive' }
+    })
+    expect(Object.isFrozen(result)).toBeTruthy()
+  })
+  test('First level obj update with undefined', () => {
+    const frozen = Object.freeze({
+      name: 'Bruce',
+      lastname: 'Waine',
+      address: { city: 'Gotham', country: 'US', street: 'Mountain Drive' }
+    })
+    const result = mutate(frozen, { name: undefined, address: { street: '1007 Mountain Drive' } })
+    expect(result.name).toBe(undefined)
+    expect(result.address.street).toBe('1007 Mountain Drive')
+    expect(result).toEqual({
+      name: undefined,
+      lastname: 'Waine',
+      address: { city: 'Gotham', country: 'US', street: '1007 Mountain Drive' }
+    })
+    expect(Object.isFrozen(result)).toBeTruthy()
+  })
+  test('Second level obj update with undefined', () => {
+    const frozen = Object.freeze({
+      name: 'Bruce',
+      lastname: 'Waine',
+      address: { city: 'Gotham', country: 'US', street: 'Mountain Drive' }
+    })
+    const result = mutate(frozen, { name: 'faa', address: { street: undefined } })
+    expect(result.name).toBe('faa')
+    expect(result.address.street).toBe(undefined)
+    expect(result).toEqual({
+      name: 'faa',
+      lastname: 'Waine',
+      address: {
+        city: 'Gotham',
+        country: 'US',
+        street: undefined
+      }
+    })
+    expect(Object.isFrozen(result)).toBeTruthy()
+  })
+
+  test('undefined for partial', () => {
+    expect(mutate({ name: 'Batman' }, undefined)).toEqual({ name: 'Batman' })
+  })
+  test('undefined for obj', () => {
+    expect(mutate({ name: 'Batman' }, undefined)).toEqual({ name: 'Batman' })
+  })
+  test('First level obj update with array', () => {
+    const frozen = Object.freeze({
+      name: ['Bruce'],
+      lastname: 'Waine',
+      address: { city: 'Gotham', country: 'US', street: 'Mountain Drive' }
+    })
+    const result = mutate(frozen, { name: ['Batman'], address: { street: '1007 Mountain Drive' } })
+    expect(result.name).toEqual(['Batman'])
+    expect(result.address.street).toBe('1007 Mountain Drive')
+    expect(result).toEqual({
+      name: ['Batman'],
+      lastname: 'Waine',
+      address: { city: 'Gotham', country: 'US', street: '1007 Mountain Drive' }
+    })
+    expect(Object.isFrozen(result)).toBeTruthy()
   })
 })
