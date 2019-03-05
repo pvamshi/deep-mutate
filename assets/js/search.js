63c932f7d4866a4f20cd6d9a5b5dc87fddef6ab4
Vamshi Krishna Pothuka <pvamshi@gmail.com>
2019-03-05 14:29:38 +0530

    Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function","65536":"Type literal","4194304":"Type alias"},"rows":[{"id":0,"kind":4194304,"name":"RecursivePartial","url":"globals.html#recursivepartial","classes":"tsd-kind-type-alias tsd-has-type-parameter"},{"id":1,"kind":65536,"name":"__type","url":"globals.html#recursivepartial.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"RecursivePartial"},{"id":2,"kind":64,"name":"mutate","url":"globals.html#mutate","classes":"tsd-kind-function tsd-has-type-parameter"}]};
